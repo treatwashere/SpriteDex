65e870b429e5055a00dab35fe77059a009c6b0ec
cloud9 <thefinalrickwashere@gmail.com>
2026-06-08 11:04:08 -0500

    Update sprites-data.js

--- sprites-data.js
+++ sprites-data.js
@@ -1,9 +1,9 @@
 // DEVELOPER DATA SHEET: Add new sprites or toggle unreleased states here.
 const baseSprites = [
-    { id: "earth_basic", name: "Earth Sprite", theme: "Basic", image: "sprites/water_basic.png", unreleased: false },
+    { id: "earth_basic", name: "Earth Sprite", theme: "Basic", image: "sprites/earth_basic.png", unreleased: false },
     { id: "earth_gold", name: "Gold Earth Sprite", theme: "Gold", image: "sprites/earth_gold.png", unreleased: false },
     { id: "earth_candy", name: "Candy Earth Sprite", theme: "Candy", image: "sprites/earth_candy.png", unreleased: true },
     { id: "earth_galaxy", name: "Galaxy Earth Sprite", theme: "Galaxy", image: "sprites/earth_galaxy.png", unreleased: true },
-    { id: "earth_gem", name: "Gem Earth Sprite", theme: "Gem", image: "sprites/earth_gem.png", unreleased: false },
-    { id: "theburntpeanut_basic", name: "TheBurntPeanut Sprite", theme: "Basic", image: "sprites/theburntpeanut_basic", unreleased: false },
+    { id: "earth_gem", name: "Gem Earth Sprite", theme: "Gem", image: "sprites/earth_gem.png", unreleased: true },
+    { id: "theburntpeanut_basic", name: "TheBurntPeanut Sprite", theme: "Basic", image: "sprites/theburntpeanut_basic.png", unreleased: false },
 ];
